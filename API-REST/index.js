d1ea7963446101dde83330ea594adf106980b9ce
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-09-30 01:21:06 -0500

    API REST - CORS

---
+++ API-REST/index.js
@@ -0,0 +1,120 @@
+const express = require('express')
+const crypto = require('node:crypto')
+const cors = require('cors')
+const { validateMovie, validateParcialMovie } = require('./schemas/movies')
+
+const MOVIES = require('./movies.json')
+
+const ACCEPTED_ORIGINS = [
+  'http://localhost:5173',
+  'http://localhost:8080',
+  'http://movies.com',
+]
+
+const app = express()
+app.disable('x-powered-by')
+app.use(express.json())
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+  })
+)
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' })
+  // const genres = MOVIES.reduce((acc, movie) => {
+  //   movie.genre.forEach((genre) => {
+  //     if (!acc.includes(genre)) {
+  //       acc.push(genre)
+  //     }
+  //   })
+  //   return acc
+  // }, [])
+  // res.json({ genres })
+})
+
+app.get('/movies', (req, res) => {
+  const origin = req.header('origin')
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.setHeader('Access-Control-Allow-Origin', origin)
+  }
+
+  const { genre } = req.query
+  if (genre) {
+    const movies = MOVIES.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    )
+    if (movies.length > 0) return res.json(movies)
+    return res.status(404).json({ message: 'Movies not found' })
+  }
+
+  res.json(MOVIES)
+})
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  if (id === 'filter') return
+  const movie = MOVIES.find((movie) => movie.id === id)
+  if (movie) {
+    res.json(movie)
+  } else {
+    res.status(404).json({ message: 'Movie not found' })
+  }
+})
+
+app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  } else {
+    const newMovie = {
+      id: crypto.randomUUID(),
+      ...result.data,
+    }
+    MOVIES.push(newMovie)
+    return res.status(201).json(result.data)
+  }
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const result = validateParcialMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const movieIndex = MOVIES.findIndex((movie) => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const newMovie = { ...MOVIES[movieIndex], ...result.data }
+  MOVIES[movieIndex] = newMovie
+  return res.json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = MOVIES.findIndex((movie) => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  MOVIES.splice(movieIndex, 1)
+
+  return res.status(204).json()
+})
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000')
+})
---
+++ API-REST/schemas/movies.js
@@ -0,0 +1,38 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    required_error: 'Title is required',
+    invalid_type_error: 'Title must be a string',
+  }),
+  year: z.number().int().min(1900).max(2025),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  poster: z.url({
+    invalid_type_error: 'Poster must be a URL',
+  }),
+  rate: z.number().min(0).max(10).default(5),
+  genre: z.array(
+    z.enum([
+      'Drama',
+      'Action',
+      'Crime',
+      'Adventure',
+      'Sci-Fi',
+      'Romance',
+      'Animation',
+      'Biography',
+      'Fantasy',
+    ])
+  ),
+})
+
+function validateMovie(input) {
+  return movieSchema.safeParse(input)
+}
+
+function validateParcialMovie(input) {
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = { validateMovie, validateParcialMovie }
